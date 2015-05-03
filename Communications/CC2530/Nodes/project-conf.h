0558b99b996b7bbc27be133e3686e4608ce7fd44
Benito Palacios Sánchez <benito356@gmail.com>
2015-05-03 03:38:35 +0200

    Add compile variables to enable banking and UART input

---
+++ Communications/CC2530/Nodes/project-conf.h
@@ -0,0 +1,23 @@
+/*
+    project-conf.h - Node project configuration.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITN+ESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this file.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+#define UART0_CONF_WITH_INPUT 1     // Enable input communication by UART0
+#define STARTUP_CONF_VERBOSE 1      // Enable statup verbosity
+
+#endif /* PROJECT_CONF_H_ */
